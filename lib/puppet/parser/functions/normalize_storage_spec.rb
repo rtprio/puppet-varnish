55cc9c10bfc2702a586c235e3a86cc3488873290
Elliot Schlegelmilch <elliot@pdx.edu>
2017-03-27 08:04:30 -0700

    update includes paths to new location.

--- lib/puppet/parser/functions/normalize_storage_spec.rb
+++ lib/puppet/parser/functions/normalize_storage_spec.rb
@@ -15,7 +15,7 @@
 # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 #
-require File.expand_path('../../../../varnish_helper', __FILE__)
+require File.expand_path('../../../../puppet_x/finalsite/varnish_helper', __FILE__)
 
 module Puppet::Parser::Functions
   newfunction(:normalize_storage_spec, type: :rvalue, doc: <<-'ENDHEREDOC') do |args|
--- lib/puppet/parser/functions/validate_storage_spec.rb
+++ lib/puppet/parser/functions/validate_storage_spec.rb
@@ -15,7 +15,7 @@
 # AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 # OR OTHER DEALINGS IN THE SOFTWARE.
 #
-require File.expand_path('../../../../varnish_helper', __FILE__)
+require File.expand_path('../../../../puppet_x/finalsite/varnish_helper', __FILE__)
 
 module Puppet::Parser::Functions
   newfunction(:validate_storage_spec, :doc => <<-'ENDHEREDOC') do |args|
